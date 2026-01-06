c7e8e8e9caaf66ddfd6319ca920fc40ac878a9d6
Margaux <margaux.challeat@grenoble-inp.org>
2026-01-06 19:23:25 +0100

    changed tests for improved log/testing readability

--- freertos/Demo/test.c
+++ freertos/Demo/test.c
@@ -2,33 +2,31 @@
 
 static void TaskA(void *arg)
 {
-    UART_printf("A");
+    UART_printf("A\n");
     vTaskDelay(pdMS_TO_TICKS(5));
 }
 
 static void TaskB(void *arg)
 {
-    UART_printf("B");
+    UART_printf("B\n");
     vTaskDelay(pdMS_TO_TICKS(18));
 }
 
 static void TaskC(void *arg)
 {
-    for (;;) {
-        UART_printf("C");
-        vTaskDelay(pdMS_TO_TICKS(100));
-    }
+    UART_printf("This is a non periodic task.\n");
+    vTaskDelay(pdMS_TO_TICKS(100));
 }
 
 void vCreateTestTasks(List_t *Periodic, List_t *NonPeriodic){
     
     vCreateAndAddTask("TaskA", TaskA, NULL, 3, true,
-                      pdMS_TO_TICKS(10), pdMS_TO_TICKS(10),
+                      pdMS_TO_TICKS(100), pdMS_TO_TICKS(100),
                       POLICY_SKIP,
                       Periodic, NonPeriodic);
 
     vCreateAndAddTask("TaskB", TaskB, NULL, 2, true,
-                      pdMS_TO_TICKS(20), pdMS_TO_TICKS(15),
+                      pdMS_TO_TICKS(200), pdMS_TO_TICKS(200),
                       POLICY_KILL,
                       Periodic, NonPeriodic);
 
