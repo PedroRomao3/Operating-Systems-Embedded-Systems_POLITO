9bb40a84dd2cdfd803f1b0f9d750eeec0c28d78a
Margaux <margaux.challeat@grenoble-inp.org>
2026-01-14 17:45:36 +0100

    Added semihosting initializer (initialise_monitor_handles) Added command line reading & example of file management

--- freertos/Demo/main.c
+++ freertos/Demo/main.c
@@ -1,9 +1,11 @@
 #include "FreeRTOS.h"
+#include "logging.h"
 #include "task.h"
 #include "setup.h"
 #include "test.h"
 #include "uart.h"
 #include "list.h"
+#include "logging_stack.h"
 
 #include <stdio.h>
 
@@ -11,10 +13,26 @@
 List_t PeriodicTaskConfigList;
 List_t NonPeriodicTaskConfigList;
 
+extern void initialise_monitor_handles(void);
+
 int main(void)
 {
     /* Initialise process lists */
     UART_init();
+
+    /* Initializes semihosting (gives access to host computer files) */
+    initialise_monitor_handles();
+
+    /* Reads the arguments given in the command line */
+    char *cmd = (char *)get_cmdline();
+
+    vLoggingPrintf(cmd);
+    UART_printf("\n");
+
+    /*  Example of writing/reading a file and testing its content 
+        You should also find the file on your pc after running these lines */
+    write_file("./Output/test.txt", "TEST");
+    print_bool(cmp_file("./Output/test.txt", "TEST")); // should print True
 
     UART_printf("1");
     vTaskListsInitialize(&PeriodicTaskConfigList, &NonPeriodicTaskConfigList);
